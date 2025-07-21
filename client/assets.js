f872a2778e01e09e546c658d9db35bfe6d87d7e4
Aidan <aidanparkhurst@gmail.com>
2025-07-21 16:17:55 -0400

    Vite img import

--- client/assets.js
+++ client/assets.js
@@ -1,14 +1,23 @@
+import brushHandImg from './assets/brushhand.png';
+import emptyHandImg from './assets/emptyhand.png';
+import brushImg from './assets/brush.png';
+import crownImg from './assets/crown.png';
+import mannyImg from './assets/goblins/manny.png';
+import stanleyImg from './assets/goblins/stanley.png';
+import rickyImg from './assets/goblins/ricky.png';
+import blimpImg from './assets/goblins/blimp.png';
+
 class Assets {
     constructor() {
         this.assets = {
-            "brush_hand": "assets/brushhand.png",
-            "empty_hand": "assets/emptyhand.png",
-            "brush": "assets/brush.png",
-            "crown": "assets/crown.png",
-            "manny": "assets/goblins/manny.png",
-            "stanley": "assets/goblins/stanley.png",
-            "ricky": "assets/goblins/ricky.png",
-            "blimp": "assets/goblins/blimp.png",
+            "brush_hand": brushHandImg,
+            "empty_hand": emptyHandImg,
+            "brush": brushImg,
+            "crown": crownImg,
+            "manny": mannyImg,
+            "stanley": stanleyImg,
+            "ricky": rickyImg,
+            "blimp": blimpImg,
         };
         this.sprites = {};
     }
